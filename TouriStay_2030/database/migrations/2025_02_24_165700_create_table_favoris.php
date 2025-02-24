ab68b9c7a40cb2e4d656b9e4f73cf6ed0a24b637
M'hamed Ouhanane <mhmdeouhnane60@gmail.com>
2025-02-24 22:01:57 +0000

    Crée la base de donnée du projet

---
+++ TouriStay_2030/database/migrations/2025_02_24_165700_create_table_favoris.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favoris', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('annonce_id');
+            $table->unsignedBigInteger('touriste_id');
+            $table->foreign('touriste_id')->references('id')->on('touristes')->onDelete('cascade');
+            $table->foreign('annonce_id')->references('id')->on('annonces')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favoris');
+    }
+};
